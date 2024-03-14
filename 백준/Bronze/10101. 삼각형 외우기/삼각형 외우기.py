4c5b51ba49c3a7aadaf0362212f88b2bc7aca96f
Jeyong <104718158+JayongLee@users.noreply.github.com>
2024-03-14 15:38:00 +0900

    [Bronze IV] Title: 삼각형 외우기, Time: 36 ms, Memory: 31120 KB -BaekjoonHub

---
+++ 백준/Bronze/10101. 삼각형 외우기/삼각형 외우기.py
@@ -0,0 +1,15 @@
+import sys
+
+A = int(input())
+B = int(input())
+C = int(input())
+
+if A == B == C == 60 :
+    print("Equilateral")
+elif A + B + C == 180 :
+    if A != B and B != C and C != A :
+        print("Scalene")
+    else :
+        print("Isosceles")
+else :
+    print("Error")
