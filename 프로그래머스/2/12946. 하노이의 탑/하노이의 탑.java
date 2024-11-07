17e7ffa7f08d32351e2f8cc67288a3a66d6fabe3
Jeyong <104718158+JayongLee@users.noreply.github.com>
2024-11-07 20:33:16 +0900

    [level 2] Title: 하노이의 탑, Time: 2.05 ms, Memory: 91.3 MB -BaekjoonHub

--- 프로그래머스/2/12946. 하노이의 탑/하노이의 탑.java
+++ 프로그래머스/2/12946. 하노이의 탑/하노이의 탑.java
@@ -3,21 +3,20 @@ import java.util.*;
 class Solution {
 
     public int[][] solution(int n) {
-        List<int[]> move = new ArrayList<>();
-        hanoi(move, n, 1, 2,3);
-        int[][] answer = move.toArray(new int[move.size()][]);
+        List<int[]> move_history = new ArrayList();
+        hanoi(move_history, n, 1, 2, 3);
+        int[][] answer = move_history.toArray(new int[move_history.size()][]);
         return answer;
     }
-
-    public void hanoi (List<int[]> move ,int n, int from, int temp, int to){
+    
+    public void hanoi(List<int[]> move_history, int n, int from, int temp, int to) {
         if (n == 1) {
-            move.add(new int[]{from, to});
+            move_history.add(new int[] {from, to});
             return;
         }
-
-        hanoi(move ,n-1, from, to, temp); // n-1개를 임시로 옮김 (목적지가 아닌곳)
-        move.add(new int[]{from, to}); // 가장 큰 원판을 목표로
-        hanoi(move,n-1, temp, from, to); // 임시로 옮긴걸 다시 목표로
+        
+        hanoi(move_history, n-1, from, to, temp);
+        move_history.add(new int[] {from, to});
+        hanoi(move_history, n-1, temp, from, to);
     }
-
 }
