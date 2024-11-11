2da1416346ad380750a6f46c0f067a000f0a2fd7
Jeyong <104718158+JayongLee@users.noreply.github.com>
2024-11-11 18:27:40 +0900

    [Bronze III] Title: 완전 세제곱, Time: 164 ms, Memory: 16640 KB -BaekjoonHub

--- 백준/Bronze/4690. 완전 세제곱/완전 세제곱.java
+++ 백준/Bronze/4690. 완전 세제곱/완전 세제곱.java
@@ -1,9 +1,3 @@
-import javax.swing.*;
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.util.*;
-
 public class Main {
     public static void main(String[] args) {
         for(int a = 3; a <= 100; a++) {
@@ -11,11 +5,7 @@ public class Main {
                 for(int c = b + 1; c <= a; c++) {
                     for(int d = c + 1; d <= a; d++) {
                         if (a * a * a == b * b * b + c * c * c + d * d * d) {
-                            if (b != c && b != d && c != d) {
-                                int[] arr = {b, c, d};
-//                                Arrays.sort(arr);
-                                System.out.println("Cube = " + a + ", Triple = (" + arr[0] + "," + arr[1] + "," + arr[2] + ")");
-                            }
+                            System.out.println("Cube = " + a + ", Triple = (" + b + "," + c + "," + d + ")");
                         }
                     }
                 }
