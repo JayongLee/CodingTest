f93bcd7a92e00fb970278f1c038b31b584b6307c
Jeyong <104718158+JayongLee@users.noreply.github.com>
2024-11-18 18:40:48 +0900

    [level 1] Title: K번째수, Time: 1.90 ms, Memory: 74.5 MB -BaekjoonHub

--- 프로그래머스/1/42748. K번째수/K번째수.java
+++ 프로그래머스/1/42748. K번째수/K번째수.java
@@ -3,19 +3,19 @@ import java.util.Arrays;
 
 class Solution {
     public int[] solution(int[] array, int[][] commands) {
-        ArrayList<Integer> ans = new ArrayList<>();
-        for (int l = 0; l < commands.length; l++) {
-            int i = commands[l][0];
-            int j = commands[l][1];
-            int k = commands[l][2];
-
-            int[] new_array = Arrays.copyOfRange(array, i - 1, j);
-            Arrays.sort(new_array);
-            ans.add(new_array[k-1]);
+        ArrayList<Integer> arr = new ArrayList<>();
+        for(int l = 0; l < commands.length; l++){
+            int i = commands[l][0] - 1; // from
+            int j = commands[l][1];  // to
+            int k = commands[l][2]; // index
+            int[] sort = Arrays.copyOfRange(array, i, j);
+            Arrays.sort(sort);
+            arr.add(sort[k-1]);
         }
-        int[] answer = new int[ans.size()];
-        for (int i = 0; i < ans.size(); i++) {
-            answer[i] = ans.get(i);
+
+        int[] answer = new int[arr.size()];
+        for (int i = 0; i < arr.size(); i++) {
+            answer[i] = arr.get(i);
         }
         return answer;
     }
