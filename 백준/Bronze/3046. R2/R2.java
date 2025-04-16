3aeb6307a07fb9bf835f51fc37a0c6ee0e39f4dc
Jeyong <104718158+JayongLee@users.noreply.github.com>
2025-04-16 12:35:29 +0900

    [Bronze IV] Title: R2, Time: 108 ms, Memory: 14148 KB -BaekjoonHub

---
+++ 백준/Bronze/3046. R2/R2.java
@@ -0,0 +1,20 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.time.LocalDateTime;
+import java.time.ZoneId;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
+import static java.time.ZoneOffset.UTC;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int N = Integer.parseInt(st.nextToken());
+        int M = Integer.parseInt(st.nextToken());
+        System.out.println(2*M - N);
+    }
+}
+
