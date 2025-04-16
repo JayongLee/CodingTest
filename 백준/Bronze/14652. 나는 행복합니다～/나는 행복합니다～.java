7b09614eac53fa7285af8a349d3c8ee0c8193875
Jeyong <104718158+JayongLee@users.noreply.github.com>
2025-04-16 12:31:29 +0900

    [Bronze IV] Title: 나는 행복합니다~, Time: 124 ms, Memory: 16168 KB -BaekjoonHub

---
+++ 백준/Bronze/14652. 나는 행복합니다～/나는 행복합니다～.java
@@ -0,0 +1,23 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.time.LocalDateTime;
+import java.time.ZoneId;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
+import static java.time.ZoneOffset.UTC;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int N = Integer.parseInt(st.nextToken());
+        int M = Integer.parseInt(st.nextToken());
+        int K = Integer.parseInt(st.nextToken());
+        int x = K / M;
+        int y = K % M;
+        System.out.println(x + " " + y);
+    }
+}
+
