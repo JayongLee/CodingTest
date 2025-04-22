2af9daf906572eb8238ef1f3570b618864b51aec
Jeyong <104718158+JayongLee@users.noreply.github.com>
2025-04-22 21:35:26 +0900

    [Bronze III] Title: A+B - 6, Time: 104 ms, Memory: 14220 KB -BaekjoonHub

---
+++ 백준/Bronze/10953. A＋B － 6/A＋B － 6.java
@@ -0,0 +1,17 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int T = Integer.parseInt(br.readLine());
+        for (int i = 0; i < T; i++) {
+            String s = br.readLine();
+            String[] s_arr = s.split(",");
+            System.out.println(Integer.parseInt(s_arr[0]) + Integer.parseInt(s_arr[1]));
+        }
+    }
+}
+
