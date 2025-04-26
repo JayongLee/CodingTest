dbaca8f8188785aa0ed90a48e122f8e63e2577d1
Jeyong <104718158+JayongLee@users.noreply.github.com>
2025-04-26 23:42:04 +0900

    [Bronze V] Title: A+B - 9, Time: 104 ms, Memory: 14396 KB, Score: 75 point -BaekjoonHub

--- 백준/Bronze/15740. A＋B － 9/A＋B － 9.java
+++ 백준/Bronze/15740. A＋B － 9/A＋B － 9.java
@@ -7,11 +7,9 @@
         public static void main(String[] args) throws IOException {
             BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
             StringTokenizer st = new StringTokenizer(br.readLine());
-            double a = Double.parseDouble(st.nextToken());
-            double b = Double.parseDouble(st.nextToken());
-            String s = String.valueOf(a+b);
-            String[] arr = s.split("\\.");
-            System.out.println(arr[0]);
+            long a = Long.parseLong(st.nextToken());
+            long b = Long.parseLong(st.nextToken());
+            System.out.println(a + b);
         }
     }
 
