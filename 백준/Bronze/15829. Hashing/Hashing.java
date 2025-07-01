b71776054a0a3bd840b992efa419cccb357d2938
Jeyong <104718158+JayongLee@users.noreply.github.com>
2025-07-01 21:56:35 +0900

    [Bronze II] Title: Hashing, Time: 108 ms, Memory: 14252 KB, Score: 100 point -BaekjoonHub

--- 백준/Bronze/15829. Hashing/Hashing.java
+++ 백준/Bronze/15829. Hashing/Hashing.java
@@ -1,17 +1,20 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.Arrays;
-import java.util.StringTokenizer;
+import java.math.BigDecimal;
 
 class Main{
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int n = Integer.parseInt(br.readLine());
         String hash = br.readLine();
-        long hash_value = 0L;
+        long hash_value = 0;
         for (int i = 0; i < hash.length(); i++){
-            hash_value += (long) ((hash.charAt(i) - 'a' + 1) * Math.pow(31, i));
+            long multi = 1;
+            for (int j = 1; j <= i; j++){
+                multi = (multi * 31) % 1234567891;
+            }
+            hash_value = (hash_value + (hash.charAt(i) - 'a' + 1) * multi) % 1234567891;
         }
         System.out.println(hash_value);
     }
