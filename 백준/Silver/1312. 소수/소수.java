c6256a7b92e77494c25bb3111a7d56ea072caeca
Jeyong <104718158+JayongLee@users.noreply.github.com>
2025-07-29 23:20:51 +0900

    [Silver V] Title: 소수, Time: 112 ms, Memory: 14204 KB -BaekjoonHub

---
+++ 백준/Silver/1312. 소수/소수.java
@@ -0,0 +1,19 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int A = Integer.parseInt(st.nextToken());
+        int B = Integer.parseInt(st.nextToken());
+        int N = Integer.parseInt(st.nextToken());
+
+        A = A % B;  // 정수 부분 제외
+        for (int i = 1; i < N; i++) {
+            A = (A * 10) % B;
+        }
+        A = (A * 10) / B;
+        System.out.println(A);
+    }
+}
