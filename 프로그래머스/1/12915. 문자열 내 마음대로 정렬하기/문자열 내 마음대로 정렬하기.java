f35689849bd4e3dd6200a8d2afd7d53539bf50c8
Jeyong <104718158+JayongLee@users.noreply.github.com>
2025-08-28 20:56:12 +0900

    [level 1] Title: 문자열 내 마음대로 정렬하기, Time: 1.07 ms, Memory: 85.8 MB -BaekjoonHub

--- 프로그래머스/1/12915. 문자열 내 마음대로 정렬하기/문자열 내 마음대로 정렬하기.java
+++ 프로그래머스/1/12915. 문자열 내 마음대로 정렬하기/문자열 내 마음대로 정렬하기.java
@@ -2,32 +2,11 @@ import java.util.*;
 
 class Solution {
     public String[] solution(String[] strings, int n) {
-
-        int[] char_sort = new int[strings.length];
-        for (int i = 0; i < char_sort.length; i++) {
-            char_sort[i] = strings[i].charAt(n) - 'A';
-        }
-        ArrayList<String> ans = new ArrayList<>();
-
-        Arrays.sort(char_sort);
-        Arrays.sort(strings);
-        for (int i = 0; i < char_sort.length; i++) {
-            for (int j = 0; j < strings.length; j++) {
-                if ((char)(char_sort[i] + 65) == strings[j].charAt(n)) {
-                    if (!ans.contains(strings[j])) {
-                        ans.add(strings[j]);
-                        break;
-                    }
-                    
-                }
-            }
-        }
-
-        String[] answer = new String[ans.size()];
-        for (int k = 0; k < ans.size(); k++) {
-            answer[k] = ans.get(k);
-        }
-
-        return answer;
+        Arrays.sort(strings, (o1, o2) -> {
+            if(o1.charAt(n) == o2.charAt(n))
+                return o1.compareTo(o2);
+            return o1.charAt(n) - o2.charAt(n);
+        });
+        return strings;
     }
 }
