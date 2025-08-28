614cd1b27a4efbc3ec6ff579586e27e2f416a086
Jeyong <104718158+JayongLee@users.noreply.github.com>
2025-08-28 21:03:30 +0900

    [Silver IV] Title: ATM, Time: 116 ms, Memory: 14476 KB -BaekjoonHub

--- 백준/Silver/11399. ATM/ATM.java
+++ 백준/Silver/11399. ATM/ATM.java
@@ -1,45 +1,34 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.ArrayList;
 import java.util.Arrays;
-import java.util.Scanner;
 import java.util.StringTokenizer;
-import java.util.TreeMap;
+import java.util.Comparator;
+import java.util.HashSet;
+import java.util.Set;
 
 public class Main {
-    public static void main(String[] args) throws IOException {
-        Scanner scanner = new Scanner(System.in);
-        int N = scanner.nextInt();
-
-        int[] time = new int[N];
-
-        for(int i = 0; i < N; i++) {
-            time[i] = scanner.nextInt();
-        }
-
-        Arrays.sort(time);
-
-//        int sum = 0;
-//
-//        for (int i = 0; i < N; i++) {
-//            sum += money_time(i, time);
-//        }
-        int sum = 0;
-        int cumulativeSum = 0;
-
-        for (int i = 0; i < N; i++) {
-            cumulativeSum += time[i];
-            sum += cumulativeSum;
-        }
-
-        System.out.println(sum);
-    }
-
-//    public static int money_time(int i, int[] arr) {
-//        if (i == 0) {
-//            return arr[0];
-//        }
-//
-//        return money_time(i-1, arr) + arr[i];
-//    }
+	
+    private static final BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    
+	public static void main(String[] args) throws IOException {
+		int N = Integer.parseInt(br.readLine());
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		int[] line = new int[N];
+		for (int i = 0; i < N; i++) {
+			line[i] = Integer.parseInt(st.nextToken());
+		}
+		
+		Arrays.sort(line);
+		
+		int subSum = 0;
+		int totalSum = 0;
+		for (int i = 0; i < N; i++) {
+			int n = line[i];
+			subSum += n;
+			totalSum += subSum;
+		}
+		System.out.print(totalSum);
+	}
 }
